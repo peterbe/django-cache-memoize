922e5e5151813c4756d011ff134ca4fc6618aed2
Peter Bengtsson <mail@peterbe.com>
2018-08-10 08:44:58 -0400

    0.1.2

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -1,3 +1,9 @@
+0.1.2
+
+- Ability to specify a different-than-default cache alias
+  Thanks @benspaulding
+  `pull#6 <https://github.com/peterbe/django-cache-memoize/pull/6>`_
+
 0.1.1
 ~~~~~
 
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ _here = path.dirname(__file__)
 
 setup(
     name='django-cache-memoize',
-    version='0.1.1',
+    version='0.1.2',
     description=(
         'Django utility for a memoization decorator that uses the Django '
         'cache framework.'
